da4eed3348e9e5ede440cd97ba5249d9cf29a3f8
Muhammad Sahal Qasim <36259752+SheikhSahal@users.noreply.github.com>
2020-11-26 18:21:26 +0500

    create class

--- quizapp/src/Quiz_service/Quiz_service.ts
+++ quizapp/src/Quiz_service/Quiz_service.ts
@@ -1,5 +1,16 @@
-export const getQuizDetails = async(totalQuestions: number, level: string) => {
+import { QuestionType, Quiz } from './../Classes/quiz_types'
+
+export const getQuizDetails = async (totalQuestions: number, level: string): Promise<Quiz[]> => {
     const res = await fetch(`Https://opentdb.com/api.php?amount=${totalQuestions}&difficulty=${level}&type=multiple`)
-    let {results} = await res.json();
+    let { results } = await res.json();
+
+    const quiz = results.map((questionObj: Quiz, ind : number) => {
+        return {
+            question: questionObj.question,
+            answer: questionObj.correct_answer,
+            option: questionObj.incorrect_answers
+        }
+    })
     return results;
-}
+} 
+
